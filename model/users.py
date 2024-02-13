04725d4a781a7d0040cb01e3cc7f73a5b42abb03
ak146 <studentgit34321@outlook.com>
2024-02-12 19:41:59 -0800

    score? score?

--- model/users.py
+++ model/users.py
@@ -78,19 +78,27 @@ class User(db.Model):
     _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
     _dob = db.Column(db.Date)
-    _perms = db.column(db.boolean, unique=False, nullable= True)
+    _score = db.Column(db.Integer, default = 0, nullable = False)
 
     # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
     posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
 
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", dob=date.today(), perms=False):
+    def __init__(self, name, uid, password="123qwerty", dob=date.today(), posts=False, score=0):
         self._name = name    # variables with self prefix become part of the object, 
         self._uid = uid
         self.set_password(password)
         self._dob = dob
-        self._perms = perms
+        self._score=score
 
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def role(self, score):
+        self._score = score
+  
     # a name getter method, extracts name from object
     @property
     def name(self):
@@ -171,12 +179,13 @@ class User(db.Model):
             "uid": self.uid,
             "dob": self.dob,
             "age": self.age,
-            "posts": [post.read() for post in self.posts]
+            "posts": [post.read() for post in self.posts],
+            "score": self.score
         }
 
     # CRUD update: updates user name, password, phone
     # returns self
-    def update(self, name="", uid="", password=""):
+    def update(self, name="", uid="", password="", score=None):
         """only updates values with length"""
         if len(name) > 0:
             self.name = name
@@ -184,6 +193,8 @@ class User(db.Model):
             self.uid = uid
         if len(password) > 0:
             self.set_password(password)
+        if score is not None:
+            self.score=score
         db.session.commit()
         return self
 
@@ -205,10 +216,10 @@ def initUsers():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10))
-        u3 = User(name='Alexander Graham Bell', uid='lex')
-        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9))
+        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11), score=0)
+        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10), score =0)
+        u3 = User(name='Alexander Graham Bell', uid='lex', score=0)
+        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9),score=0)
         users = [u1, u2, u3, u4]
 
         """Builds sample user/note(s) data"""
